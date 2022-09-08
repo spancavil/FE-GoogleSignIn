748b33124beea25f02a93f101dfbf4543815317c
spancavil <soancavil@gmail.com>
2022-09-08 15:53:57 -0300

    Added facebook and apple

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-    const URL_BASE = "http://localhost:8888/api/v1/";
+    const URL_BASE = "https://grandest-fe.herokuapp.com/api/v1/";
     const GOOGLE_CLIENT_ID =
         "989227771619-nifl5ep09cfqo06ncsrj46k4r2p10gkc.apps.googleusercontent.com";
 
-    const [google, setGoogle] = useState(window.google);
+    const [google] = useState(window.google);
+    const [apple] = useState(window.AppleID);
+    const [facebook] = useState(window.FB);
     /* const handleSignIn = () => {
         window.open(URL_BASE + "/auth/signin/google", "_self");
     }; */
@@ -38,7 +40,7 @@ function App() {
         checkAuth();
     }, []); */
 
-    async function handleCredentialResponse(response) {
+    async function handleGoogleCredential(response) {
         try {
             console.log("Encoded JWT ID token: " + response.credential);
             console.log(response);
@@ -56,7 +58,7 @@ function App() {
         if (google) {
             google.accounts.id.initialize({
                 client_id: GOOGLE_CLIENT_ID,
-                callback: handleCredentialResponse,
+                callback: handleGoogleCredential,
             });
             google.accounts.id.renderButton(
                 document.getElementById("buttonDiv"),
@@ -64,11 +66,54 @@ function App() {
             );
             // google.accounts.id.prompt(); // also display the One Tap dialog
         }
-    }, [google]);
+        if (apple) {
+            apple.auth.init({
+                clientId: "[CLIENT_ID]",
+                scope: "[SCOPES]",
+                redirectURI: "[REDIRECT_URI]",
+                state: "[STATE]",
+                nonce: "[NONCE]",
+                usePopup: true,
+            });
+        }
+    }, [google, apple]);
+
+    facebook.getLoginStatus(function(response) {
+        console.log(response);
+    });
+
+    //Listeners for apple data
+    useEffect(() => {
+        document.addEventListener("AppleIDSignInOnSuccess", (event) => {
+            // Handle successful response.
+            console.log(event.detail.data);
+        });
+
+        // Listen for authorization failures.
+        document.addEventListener("AppleIDSignInOnFailure", (event) => {
+            // Handle error.
+            console.log(event.detail.error);
+        });
+    }, []);
 
     return (
         <div className="App">
             <div id="buttonDiv"></div>
+            <div
+                id="appleid-signin"
+                data-color="black"
+                data-border="true"
+                data-type="sign in"
+            ></div>
+            <div
+                className="fb-login-button"
+                data-width=""
+                data-size="medium"
+                data-button-type="login_with"
+                data-layout="rounded"
+                data-auto-logout-link="false"
+                data-use-continue-as="false"
+            ></div>
         </div>
     );
 }
